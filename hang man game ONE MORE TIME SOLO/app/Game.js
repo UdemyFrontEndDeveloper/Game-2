a583f472c6001b30058caa96008c718493315e4d
Paweł Piontkowski <piontkowski35@gmail.com>
2021-03-21 14:48:56 +0100

    creating guess and modify start functions,

--- hang man game ONE MORE TIME SOLO/app/Game.js
+++ hang man game ONE MORE TIME SOLO/app/Game.js
@@ -1,11 +1,35 @@
 class Game{
-    // constructor()
+    constructor({output,word,category,letters}){
+        this.output = output;
+        this.word = word;
+        this.category = category;
+        this.letters = letters;
+    }
+
+    guess(labelButton){
+        console.log(labelButton)
+    }
 
     start() {
-        console.log("startujemy");
+        for(let i=0; i<26; i++){
+        const labelButton = (i+10).toString(36);
+        // console.log(labelButton);
+        const button = document.createElement("button");
+        button.innerHTML = labelButton;
+        button.addEventListener("click", () => this.guess(labelButton));
+        this.letters.appendChild(button);
+        }
     }
 }
 
-const game = new Game();
+
+
+const game = new Game({
+    output: document.getElementById("output"),
+    word: document.getElementById("word"),
+    category: document.getElementById("category"),
+    letters: document.getElementById("letters"),
+});
 
 game.start();
+
