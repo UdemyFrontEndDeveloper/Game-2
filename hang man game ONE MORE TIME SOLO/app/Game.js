bd801ab57aceb3b9d4458001d5c3b8d88cf43f24
Paweł Piontkowski <piontkowski35@gmail.com>
2021-03-21 15:10:52 +0100

    add class Quote, export from Quote.js, add quotes

--- hang man game ONE MORE TIME SOLO/app/Game.js
+++ hang man game ONE MORE TIME SOLO/app/Game.js
@@ -1,17 +1,41 @@
+import {Quote} from "./Quote.js"
+
 class Game{
+
+    quotes=[{
+        text: "bla bla bla",
+        categoryQ: "siemano",
+    },
+        {
+        text: "ble ble ble",
+        categoryQ: "witam",
+    },
+        {
+        text: "blu blu blu",
+        categoryQ: "dobry",
+    },
+        {
+        text: "bli bli bli",
+        categoryQ: "elo",
+    }];
+
     constructor({output,word,category,letters}){
         this.output = output;
         this.word = word;
         this.category = category;
         this.letters = letters;
+
+        const {text,categoryQ}= this.quotes[Math.floor(Math.random()*this.quotes.length)];
+        this.category.innerHTML = categoryQ;
+        this.word.innerHTML = new Quote(text);
     }
 
     guess(labelButton){
         console.log(labelButton)
     }
 
-    start() {
-        for(let i=0; i<26; i++){
+    creatingLetters(){
+     for(let i=0; i<26; i++){
         const labelButton = (i+10).toString(36);
         // console.log(labelButton);
         const button = document.createElement("button");
@@ -19,6 +43,10 @@ class Game{
         button.addEventListener("click", () => this.guess(labelButton));
         this.letters.appendChild(button);
         }
+    }
+
+    start() {
+       this.creatingLetters();
     }
 }
 
